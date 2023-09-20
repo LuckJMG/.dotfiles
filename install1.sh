b71edc18975b5bd65289d81469355eab51dce6be
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2023-09-20 15:29:21 -0300

    fix: rename packages to Packages on git clone

--- install1.sh
+++ install1.sh
@@ -13,10 +13,12 @@ ln -s "$LIB_PATH"/.gitconfig ~/.gitconfig
 mkdir Projects Packages 
 
 # Packages
+sudo dnf install ripgrep nodejs util-linux-user  # Dependencies
 sudo dnf install neovim zoxide exa bat zsh
+
 git clone https://github.com/LuckJMG/Neovim-Config.git ~/.config/nvim
-git clone https://github.com/dracula/zsh.git ~/packages/dracula-zsh
-git clone https://github.com/dracula/zsh-syntax-highlighting.git ~/packages/dracula-zsh-syntax-highlighting
+git clone https://github.com/dracula/zsh.git ~/Packages/dracula-zsh
+git clone https://github.com/dracula/zsh-syntax-highlighting.git ~/Packages/dracula-zsh-syntax-highlighting
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
