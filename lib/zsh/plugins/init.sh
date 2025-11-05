ab1d1f76adff53032e159bff0da682cbad036b8c
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2025-11-05 13:28:38 -0300

    fix: alias correctly grep

--- lib/zsh/plugins/init.sh
+++ lib/zsh/plugins/init.sh
@@ -1,9 +1,17 @@
 PLUGINS=$DOTFILES/plugins
 
-source $PLUGINS/bat.sh
-source $PLUGINS/eza.sh
+source "$PLUGINS/bat.sh"
+source "$PLUGINS/eza.sh"
 
 eval "$(zoxide init zsh --cmd cd)"
-alias grep=rg
 alias gsync="$SCRIPTS/gsync/gsync.sh"
+
+function grep {
+    if [ -t 1 ]; then
+        rg --smart-case "$@"
+    else
+        command grep "$@"
+    fi
+}
+alias grep=grep
 
