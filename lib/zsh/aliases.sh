b2f6f99c4c586b3e48f7b2cb77bd85a5473e3916
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2025-11-05 13:47:35 -0300

    fix: vim alias with zoxide in its own alias

--- lib/zsh/aliases.sh
+++ lib/zsh/aliases.sh
@@ -1,13 +1,21 @@
 # System
-alias reset="source $HOME/.zshrc"
+alias zsource="source $HOME/.zshrc"
+alias vim=nvim
 
 # Editor
-function vim {
-    if z "$1" 2>/dev/null; then
-        nvim .
-    else
-        nvim "$1"
-    fi
-}
+function zvim {
+	if [[ -z $1 ]]; then
+		nvim
+		return
+	fi
 
+	z_error=$(zoxide "$1" 2>&1)
+	if [[ $z_error == "zoxide: no match found" ]]; then
+		nvim "$1"
+		return
+	fi
+
+	cd "$1" || return
+	nvim .
+}
 
