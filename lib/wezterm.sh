b2be35323ef8a46a3e546c5e8515c3acdddff3b4
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-01-20 19:17:32 -0300

    feat: remove wezterm.sh

--- lib/wezterm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-wezterm() {
-	local windowsConfigPath="/mnt/c/Users/luck/.wezterm.lua"
-	local linuxConfigPath="$HOME/.config/wezterm/wezterm.lua"
-
-	"$EDITOR" "$windowsConfigPath"
-	cp "$windowsConfigPath" "$linuxConfigPath"
-}
