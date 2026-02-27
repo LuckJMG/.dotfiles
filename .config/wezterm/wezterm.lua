49f864e5a56516304b7a35ea3e57ef788ca94df6
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-02-26 23:15:32 -0300

    fix: show wezterm windows bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,17 +2,18 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- UI
+config.front_end = "WebGpu"
 config.font_size = 10
 config.color_scheme = "Everforest Dark (Gogh)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.default_cursor_style = "SteadyBar"
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.95
 
 -- Options
 config.scrollback_lines = 10000
 config.window_close_confirmation = "NeverPrompt"
-config.window_decorations = "RESIZE"
 
 -- Profiles
 config.default_domain = "WSL:Ubuntu"
